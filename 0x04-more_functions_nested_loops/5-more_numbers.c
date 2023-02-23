db38793c161620e60b32acbe3e80ee2fb8c5cd55
WAKIOM <agnessmanga@outlook.com>
2023-02-23 05:00:19 -0800

    program prints a line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,10 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
-		for (j =0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 			{
 			if (j > 9)
 			{
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints a line if n is more that 0
+ * @n: the number of lines to draw
+ * return void
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (x = 0; x < n; x++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
