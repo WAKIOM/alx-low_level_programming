bd121ac9802264b0392fbfdecb0ce725a6c6f7cd
WAKIOM <agnessmanga@outlook.com>
2023-02-23 08:39:10 -0800

    debugging 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for negative or positive
+ * @i: the number to be checked
+ * return: always zero
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+	printf("%d is positive\n", i);
+}
