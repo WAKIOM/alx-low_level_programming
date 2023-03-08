96efc299d14c0ee0ca2097d468e1e0856bcca6a6
WAKIOM <agnessmanga@outlook.com>
2023-03-08 01:10:37 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - Print a string in reverse
+  * @s: the string to reverse
+  *
+  * Return: Nothing.
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
