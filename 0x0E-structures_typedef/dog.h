f0ac32c831eafdb468fb45f2f1d29233222100d3
WAKIOM <agnessmanga@outlook.com>
2023-03-20 11:56:44 -0700

    define struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: dogs name
+ * @age: float
+ * @owner: owner
+ */
+struct dog
+	{char *name;
+	float age;
+	char *owner;
+	};
+#endif
