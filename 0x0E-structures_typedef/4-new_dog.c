8be86a55bdcd9b2796f4ec91bbba4b4485497df0
WAKIOM <agnessmanga@outlook.com>
2023-03-21 07:47:50 -0700

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,33 +1,52 @@
 #include "dog.h"
-#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - is a function that creates a new dog
+ * @name: is a pointer to name
+ * @age: is a pointer to age
+ * @owner: is a pointer to owner
  *
- * Return: pointer to new dog
+ * Return: pointer to new_dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	int i = 0;
+	int j = 0;
+	int m;
+	dog_t *dog1;
 
-	if (!name)
-		return (NULL);
-	if (!age)
-		return (NULL);
-	if (!owner)
-		return (NULL);
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 
-	d = malloc(sizeof(dog_t));
-	if (!d)
+	dog1 = malloc(sizeof(dog_t));
+	if (dog1 == NULL)
+	{
+		free(dog1);
 		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	return (d);
+	}
+	dog1->name = malloc(i * sizeof(dog1->name));
+	if (dog1->name == NULL)
+	{
+		free(dog1->name);
+		return (NULL);
+	}
+	for (m = 0; m <= i; m++)
+		dog1->name[m] = name[m];
+	dog1->age = age;
+	dog1->owner = malloc(j * sizeof(dog1->owner));
+	if (dog1->owner == NULL)
+	{
+		free(dog1->owner);
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	for (m = 0; m <= j; m++)
+		dog1->owner[m] = owner[m];
+	return (dog1);
 }
