b0b73c397af311645285db66b7cab422cb25b433
WAKIOM <agnessmanga@outlook.com>
2023-03-30 11:33:25 -0700

    made some changes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,7 @@
  * Return: no return.
  */
 
-void before_main (void)  __attribute__ ((constructor));
-void before_main(void)
+void __attribute__ ((constructor)) before_main ()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
