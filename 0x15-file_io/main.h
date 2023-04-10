aaa1bdef4ecbc2ab73dca9ca8f6bde23cbfc074d
WAKIOM <agnessmanga@outlook.com>
2023-04-10 20:19:29 +0300

    header file update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,8 +8,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
